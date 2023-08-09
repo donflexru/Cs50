f38f6e70bd79fae51970b20fa2f54585937da551
donflexru <donflexru@gmail.com>
2023-08-09 13:38:19 +0100

    added new task

--- project.py
+++ project.py
@@ -7,3 +7,12 @@ arr=[]
 arr.append(["April","girdle"])
 print(arr)
 print("wonderful")
+
+n=int(input("Number :"))
+if n > 0: 
+  print("number is positive!") 
+elif n < 0:
+  print("number is negative")
+else:
+  print("number is zero")                
+
